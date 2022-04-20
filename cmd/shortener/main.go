65fc7dc25a5a474259f2326e729867178292974c
Zhuravlev Dmitrii <Dima15129@gmail.com>
2022-04-20 21:45:02 +0700

    fix bug v2

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"github.com/ZhuravlevDmi/serviceURL/internal/config"
 	"github.com/ZhuravlevDmi/serviceURL/internal/handlers"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", handlers.MainHandler)
-	http.ListenAndServe(config.Port, nil)
+	http.ListenAndServe(":8080", nil)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,6 @@
 package config
 
 var Port = ":8080"
+var host = "localhost"
+var transferProtocol = "http"
+var ServerURL = transferProtocol + "://" + host + Port
